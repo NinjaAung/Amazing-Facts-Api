3f1cf00d60b4e7db212e192046e5f63e6d68bf29
ninjaaung <ninja.aung02@gmail.com>
2020-03-06 02:39:29 -0800

    Added search by id

--- routes/posts.js
+++ routes/posts.js
@@ -26,4 +26,14 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.get('/:postId', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.postId);
+        res.json(post);
+    } catch (err) {
+        res.json({message: err});
+    }
+});
+
+
 module.exports = router;
