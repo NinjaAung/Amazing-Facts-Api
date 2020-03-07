6a2c52c5153a2c73d3a91e298de10dc0a8fdd568
ninjaaung <ninja.aung02@gmail.com>
2020-03-06 23:26:15 -0800

    Added Salt Hashing

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const auth = express.Router();
+const bcrypt = require('bcrypt')
 
 const UserModel = require('../models/user');
 
 const Joi = require('@hapi/joi');
-const { registrationValidate } = require('../validation');
+const { registrationValidate, loginValidate } = require('../validation');
 
 
 auth.post('/register', async (req, res) => {
@@ -15,16 +16,27 @@ auth.post('/register', async (req, res) => {
     const usernameExist = await UserModel.findOne({username: req.body.username})
     if (usernameExist) return res.status(400).send('Username Taken')
 
+
+    const salt = await bcrypt.genSalt(10);
+    const hashpassword = await bcrypt.hash(req.body.password, salt);
     const user = new UserModel({
         username: req.body.username,
-        password: req.body.password
+        password: hashpassword
     })
     try{
         const savedUser = await user.save();
-        res.json(savedUser)
+        res.status(200).json({user: user._id})
     }catch (err) {
         res.status(400).send(err)
     }
 });
+
+
+auth.post('/login' ,async (req, res) => {
+    const { error } = loginValidate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+})
+
 
 module.exports = auth;
